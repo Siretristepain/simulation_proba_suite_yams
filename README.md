562b4dfb0bfa91e0015cb8db8d41f4e01bc159e0
Raphael Ollier <raphael.ollier@ipsl.fr>
2024-08-08 14:29:21 +0200

    Simulation grande suite yams v1 (Raphael).

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# simulation_proba_suite_yams
+# Simulation_proba_suite_yams
+
+Une des combinaison à réaliser au Yams est la grande suite.
+
+Il existe 2 moyens de la réaliser : 1, 2, 3, 4, 5 ou 2, 3, 4, 5, 6.
+
+Chaque joueur dispose de 3 lancers pour effectuer sa combinaison.
+
+La question est la suivante :
+
+Lorsque j'obiens 1, 3, 4, 5, 6 à mon premier lancer, quelle est la meilleure stratégie à adopter pour faire une grande suite.
+
+Voici les options :
+
+- relancer jusqu'à 2 fois le 1 pour essayer de faire un 2.
+- relancer jusqu'à 2 fois le 6 pour essayer de faire un 2 (revient au même qu'au dessus).
+- relancer le 1 ET le 6 pour maximiser mes chances de faire un 2. A partir de là j'aurais 1 chances sur 3 de faire un 1 ou un 6 à mon 3ème lancé.
+
---
+++ simulation.py
@@ -0,0 +1,75 @@
+import random
+
+def lancer_un_des():
+    result = random.randint(1,6)
+    return result
+
+def check_for_two(lancer: list):
+    if not 2 in lancer:
+        return False
+    return True
+
+def check_for_win(lancer: list):
+    lancer = sorted(lancer)
+    if (lancer == [1,2]) or (lancer == [2,6]):
+        return True
+    return False
+
+def main():
+    lancer = []
+    des_1 = lancer_un_des()
+    des_2 = lancer_un_des()
+
+    lancer.append(des_1)
+    lancer.append(des_2)
+
+    # On vérifie si on fait un lancé parfait du 1er coup
+    if check_for_win(lancer=lancer) == True:
+        return 1
+
+    # On vérifie si on a un 2
+    if check_for_two(lancer=lancer) == True:
+        # On relance le dernier dés dans l'espoir de faire un 1 ou un 6
+        dernier_lancer = lancer_un_des()
+        if dernier_lancer in [1,6]:
+            return 1
+        else:
+            return 0
+    
+    # Si on a pas de 2 :
+    # On regarde si on a un 1 ou un 6, auquel cas on lance le dernier dés dans l'espoir de faire un 2 (1/6 chance)
+    if (1 in lancer) or (6 in lancer):
+        dernier_lancer = lancer_un_des()
+        if lancer_un_des == 2:
+            return 1
+        else:
+            return 0
+    # Si on a ni 1 ni 6 on relance les 2 dés dans l'espoir de faire un lancé parfait
+    else:
+        dernier_lancer = []
+
+        dernier_lancer_des_1 = lancer_un_des()
+        dernier_lancer_des_2 = lancer_un_des()
+
+        dernier_lancer.append(dernier_lancer_des_1)
+        dernier_lancer.append(dernier_lancer_des_2)
+
+        if check_for_win(lancer=dernier_lancer) == True:
+            return 1
+        else:
+            return 0
+
+
+
+
+
+
+if __name__ == '__main__':
+    # print(lancer_un_des())
+    # print(check_for_two([1,4]))
+    # print(check_for_win([2,6]))
+    result =  []
+    for i in range(10):
+        result.append(main())
+    
+    print(sum(result))
